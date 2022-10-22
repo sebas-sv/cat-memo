390e3b0c8add8036f0284c933c358373b8c0475f
Sebastián Solís <sebas.solis.dev@gmail.com>
2022-10-22 14:35:13 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import { useState, useEffect } from "react";
+import Images from './Images';
+import './App.css';
+
+function App() {
+  const [isInit, setIsInit] = useState(true);
+  const [cards, setCards] = useState(shuffleImages(Images));
+  const [clicks, setClicks] = useState(0);
+  const [isFinish, setIsFinish] = useState(false);
+  const [activeCards, setActiveCards] = useState([]);
+  const [foundPairs, setFoundPairs] = useState([]);
+
+  useEffect(() => {
+    if (isInit) {
+      setTimeout(() => {
+        setIsInit(false);
+      }, 3000);
+    }
+  }, [isInit]);
+
+  function shuffleImages(Images) {
+    const allImages = [...Images, ...Images];
+    return allImages.sort(() => 0.5 - Math.random());
+  }
+
+  function handleClick(index) {
+    if (isFinish) {
+      resetGame();
+    }
+
+    const isActive = activeCards.includes(index);
+    const isFound = foundPairs.includes(index);
+
+    if (isInit || isActive || isFound) {
+      return;
+    }
+
+    if (activeCards.length === 0) {
+      setActiveCards([index]);
+
+      setClicks(clicks + 1);
+    }
+
+    if (activeCards.length === 1) {
+      setActiveCards([...activeCards, index]);
+
+      const firstImage = cards[activeCards[0]];
+      const secondImage = cards[index];
+
+      if (foundPairs.length + 2 === cards.length) {
+        setIsFinish(true);
+      }
+
+      if (firstImage === secondImage) {
+        setFoundPairs([...foundPairs, activeCards[0], index]);
+      }
+
+      setTimeout(() => {
+        setActiveCards([]);
+      }, 800);
+
+      setClicks(clicks + 1);
+    }
+  }
+
+  function resetGame() {
+    setCards(shuffleImages(Images));
+    setFoundPairs([]);
+    setIsFinish(false);
+    setClicks(0);
+    setIsInit(true);
+  }
+
+
+  return (
+    <div>
+
+      <div className="board">
+        {cards.map((card, index) => {
+          const iShownStyle = (activeCards.includes(index) || foundPairs.includes(index) || isInit) ? ' flipped' : '';
+          const isFoundStyle = foundPairs.includes(index) ? ' found' : '';
+
+          return (
+            <div className={"card-outer" + iShownStyle + isFoundStyle} onClick={() => handleClick(index)}>
+              <div className="card">
+                <div className="frontside">
+                  <img src={card} alt="" />
+                </div>
+                <div className="backside" />
+              </div>
+            </div>
+          );
+
+        })}
+      </div>
+
+      <div className="stats">
+        {isFinish && (
+          <div className="text">
+            <p>You have found all cats in {clicks} clicks, congratulations! Click any card to play again</p>
+          </div>
+        )}
+        {!isFinish && (
+          <div className="text">
+            <p>{clicks} clicks and {foundPairs.length / 2} found cats</p>
+          </div>
+        )}
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Images.js
@@ -0,0 +1,29 @@
+import Image01 from './img/01.jpg';
+import Image02 from './img/02.jpg';
+import Image03 from './img/03.jpg';
+import Image04 from './img/04.jpg';
+import Image05 from './img/05.jpg';
+import Image06 from './img/06.jpg';
+import Image07 from './img/07.jpg';
+import Image08 from './img/08.jpg';
+import Image09 from './img/09.jpg';
+import Image10 from './img/10.jpg';
+import Image11 from './img/11.jpg';
+import Image12 from './img/12.jpg';
+import Image13 from './img/13.jpg';
+import Image14 from './img/14.jpg';
+import Image15 from './img/15.jpg';
+import Image16 from './img/16.jpg';
+import Image17 from './img/17.jpg';
+import Image18 from './img/18.jpg';
+import Image19 from './img/19.jpg';
+import Image20 from './img/20.jpg';
+
+const Images = [
+    Image01, Image02, Image03, Image04, Image05,
+    Image06, Image07, Image08, Image09, Image10,
+    Image11, Image12, Image13, Image14, Image15,
+    Image16, Image17, Image18, Image19, Image20
+];
+
+export default Images;
